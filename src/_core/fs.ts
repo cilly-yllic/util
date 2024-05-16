06dfa5c7046855cd8b0c5714c17ecdc97918cb6d
yoshihiro okuda <yoshi.private.acc@gmail.com>
2024-05-16 09:49:25 +0900

    add method: getAllFiles

--- src/_core/fs.ts
+++ src/_core/fs.ts
@@ -1,11 +1,11 @@
 import { statSync, readdirSync } from 'fs'
 
-export const getFiles = (dir: string, _files: string[] = []) => {
+export const getAllFiles = (dir: string, _files: string[] = []) => {
   const files = readdirSync(dir)
   for (const file of files) {
     const name = dir + '/' + file
     if (statSync(name).isDirectory()) {
-      getFiles(name, _files)
+      getAllFiles(name, _files)
     } else {
       _files.push(name)
     }
