59f4fa849127b3c1afaf43f80ead8b67ebead47f
yoshihiro okuda <yoshi.private.acc@gmail.com>
2024-05-26 17:45:01 +0900

    add depth option

--- src/_core/__tests__/fs/get-all-files.spec.ts
+++ src/_core/__tests__/fs/get-all-files.spec.ts
@@ -1,8 +1,8 @@
 import { resolve, join } from 'path'
-import { getAllFiles } from '../fs.js'
+import { getAllFiles } from '../../fs.js'
 
 describe('getAllFiles', () => {
-  const path = resolve(__dirname, '../../..', 'fs-test')
+  const path = resolve(__dirname, '../../../..', 'fs-test')
   it('no exclude & include', () => {
     const exclude: string[] = []
     const include: string[] = []
@@ -15,7 +15,7 @@ describe('getAllFiles', () => {
       join(path, 'hoge/foo.ts'),
       join(path, 'hoge.ts'),
     ]
-    expect(JSON.stringify(getAllFiles(path, [], { include, exclude }))).toBe(JSON.stringify(expects))
+    expect(JSON.stringify(getAllFiles(path, { include, exclude }))).toBe(JSON.stringify(expects))
   })
   describe('set exclude', () => {
     it('filename', () => {
@@ -28,7 +28,7 @@ describe('getAllFiles', () => {
         join(path, 'hoge/foo.ts'),
         join(path, 'hoge.ts'),
       ]
-      expect(JSON.stringify(getAllFiles(path, [], { include, exclude }))).toBe(JSON.stringify(expects))
+      expect(JSON.stringify(getAllFiles(path, { include, exclude }))).toBe(JSON.stringify(expects))
     })
     it('dirname', () => {
       const exclude: string[] = ['**/piyo/**/*']
@@ -40,7 +40,7 @@ describe('getAllFiles', () => {
         join(path, 'hoge/foo.ts'),
         join(path, 'hoge.ts'),
       ]
-      expect(JSON.stringify(getAllFiles(path, [], { include, exclude }))).toBe(JSON.stringify(expects))
+      expect(JSON.stringify(getAllFiles(path, { include, exclude }))).toBe(JSON.stringify(expects))
     })
   })
   describe('set include', () => {
@@ -48,7 +48,7 @@ describe('getAllFiles', () => {
       const exclude: string[] = []
       const include: string[] = ['**/index.ts']
       const expects = [join(path, 'foo/piyo/index.ts'), join(path, 'hoge/foo/piyo/index.ts')]
-      expect(JSON.stringify(getAllFiles(path, [], { include, exclude }))).toBe(JSON.stringify(expects))
+      expect(JSON.stringify(getAllFiles(path, { include, exclude }))).toBe(JSON.stringify(expects))
     })
     it('dirname', () => {
       const exclude: string[] = []
@@ -59,7 +59,7 @@ describe('getAllFiles', () => {
         join(path, 'hoge/foo/piyo/index.ts'),
         join(path, 'hoge/foo/piyo.ts'),
       ]
-      expect(JSON.stringify(getAllFiles(path, [], { include, exclude }))).toBe(JSON.stringify(expects))
+      expect(JSON.stringify(getAllFiles(path, { include, exclude }))).toBe(JSON.stringify(expects))
     })
   })
   describe('set include & exclude', () => {
@@ -67,7 +67,24 @@ describe('getAllFiles', () => {
       const exclude: string[] = ['**/p*.ts']
       const include: string[] = ['**/foo/**/*']
       const expects = [join(path, 'foo/piyo/index.ts'), join(path, 'hoge/foo/piyo/index.ts')]
-      expect(JSON.stringify(getAllFiles(path, [], { include, exclude }))).toBe(JSON.stringify(expects))
+      expect(JSON.stringify(getAllFiles(path, { include, exclude }))).toBe(JSON.stringify(expects))
+    })
+  })
+  describe('set depth', () => {
+    it('1', () => {
+      const exclude: string[] = []
+      const include: string[] = []
+      const depth = 1
+      const expects = [join(path, 'foo.ts'), join(path, 'hoge.ts')]
+      expect(JSON.stringify(getAllFiles(path, { include, exclude, depth }))).toBe(JSON.stringify(expects))
+    })
+    it('0: toThrow', () => {
+      const exclude: string[] = []
+      const include: string[] = []
+      const depth = 0
+      expect(() => getAllFiles(join(path, 'foo'), { include, exclude, depth })).toThrow(
+        'Invalid depth, must be greater than 0'
+      )
     })
   })
 })
